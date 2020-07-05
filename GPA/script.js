0a8ede98baec37b6b3face1a620dd327a2e6b9a8
Tuan <47839860+Tuanm@users.noreply.github.com>
2020-07-05 11:05:01 +0700

    Add files via upload

--- GPA/script.js
+++ GPA/script.js
@@ -1,5 +1,70 @@
 console.log("Đã có cái mẹ gì đâu -_-");
 
+let danhSach = [];
+let tongTinChi = 0;
+let GPA = 0;
+let table = "";
+
+function inDanhSach() {
+    console.clear();
+    console.table(danhSach);
+    console.log("GPA: " + GPA);
+
+    table = "<table>";
+    table += "<tr><th>Học phần</th><th>Tín chỉ</th><th>Trọng số</th><th>Điểm quá trình</th>" + 
+                 "<th>Điểm kết thúc</th><th>Điểm tổng kết</th><th>Xếp loại</th></tr>";
+    for (var stt = 0; stt < danhSach.length; stt++) {
+        table += "<tr><td>" + danhSach[stt].ten + "</td><td>" + danhSach[stt].tinChi + "</td><td>" +
+                              danhSach[stt].trongSo + "</td><td>" + danhSach[stt].diemQT + "</td><td>" + 
+                              danhSach[stt].diemKT + "</td><td>" + danhSach[stt].diemTong + "</td><td>" + 
+                              danhSach[stt].xepLoai +
+                 "</td></tr>"
+    }
+    table += "</table>";
+
+    document.getElementById("table").innerHTML = table;
+    document.getElementById("gpa").innerHTML = "GPA: " + GPA;
+}
+
+function themDanhSach() {
+    var hocPhan = {
+        ten: "",
+        tinChi: 0,
+        trongSo: 0,
+        diemQT: 0,
+        diemKT: 0,
+        diemTong: 0,
+        xepLoai: "F",
+        diemQuyDoi: "0"
+    };
+
+    hocPhan.ten = document.getElementById("hocphan").value;
+    hocPhan.tinChi = document.getElementById("tinchi").value;
+    hocPhan.trongSo = getTrongSo();
+    hocPhan.diemQT = document.getElementById("quatrinh").value;
+    hocPhan.diemKT = document.getElementById("ketthuc").value;
+    hocPhan.diemTong = Math.round((hocPhan.diemQT * hocPhan.trongSo + 
+                                   hocPhan.diemKT * (1 - hocPhan.trongSo)) * 10) / 10;
+    hocPhan.xepLoai = (hocPhan.diemQT < 3 || hocPhan.diemKT < 3) ? "F" : getXepLoai(hocPhan.diemTong);
+    hocPhan.diemQuyDoi = getDiemQuyDoi(hocPhan.xepLoai);
+    
+    for (var stt = 0; stt < danhSach.length; stt++) {
+        if (danhSach[stt].ten === hocPhan.ten) {
+            danhSach.splice(stt, 1);
+        }
+    }
+    danhSach.push(hocPhan);
+    tongTinChi += parseInt(hocPhan.tinChi);
+
+    var tong = 0;
+    for (var stt = 0; stt < danhSach.length; stt++) {
+        tong += danhSach[stt].diemQuyDoi * danhSach[stt].tinChi;
+    }
+
+    GPA = Math.round((tong / tongTinChi) * 100) / 100;
+}
+
+
 function getTrongSo() {
     switch (document.getElementById("trongso").value) {
         case "trongso5": return 0.5;
@@ -20,6 +85,20 @@ function getXepLoai(ketQua) {
     if (ketQua >= 5) return "D+";
     if (ketQua >= 4) return "D";
     return "F";
+}
+
+function getDiemQuyDoi(xepLoai) {
+    switch (xepLoai) {
+        case "A+":
+        case "A": return 4;
+        case "B+": return 3.5;
+        case "B": return 3;
+        case "C+": return 2.5;
+        case "C": return 2;
+        case "D+": return 1.5;
+        case "D": return 1;
+    }
+    return 0;
 }
 
 var tinhDiem = document.getElementById("tinhdiem");
@@ -48,4 +127,7 @@ tinhDiem.onclick = function() {
     document.getElementById("ketqua").style.display = "inline";
     document.getElementById("xeploai").innerHTML = "</br>" + "Xếp loại" + "</br>" + xepLoai + "</br>";
     document.getElementById("xeploai").style.display = "inline";
+
+    themDanhSach();
+    inDanhSach();
 }
